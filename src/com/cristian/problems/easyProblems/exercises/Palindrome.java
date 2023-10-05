06ece15d423dd77f3c7ac1c9be30d31ac799a83b
Cristian-Lopez <126030346+CristianLopez3@users.noreply.github.com>
2023-10-05 18:15:41 -0500

    add palindrome number exercise, ease

---
+++ src/com/cristian/problems/easyProblems/exercises/Palindrome.java
@@ -0,0 +1,24 @@
+package com.cristian.problems.easyProblems.exercises;
+
+public class Palindrome {
+
+    public static boolean palindrome(int x){
+        // negative numbers aren't palindrome
+        if(x < 0) return false;
+        String numberStr = String.valueOf(x);
+        int left = 0;
+        int right = numberStr.length() - 1;
+
+        while(left < right) {
+            if(numberStr.charAt(left) != numberStr.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(palindrome(123));
+    }
+
+}
