f4fd273af9f0eef453acb32475ac1aae350b78f4
cristian <cristian.c.lopez.m@gmail.com>
2024-01-06 22:57:42 -0500

    Find the first occurrence in a haystack needle

---
+++ src/com/cristian/problems/easyProblems/tests/FirstOccurrenceIndexTest.java
@@ -0,0 +1,13 @@
+package com.cristian.problems.easyProblems.tests;
+
+import com.cristian.problems.easyProblems.exercises.FirstOccurrenceIndex;
+
+public class FirstOccurrenceIndexTest {
+
+    public static void main(String[] args){
+        String haystack = "baddasadlest";
+        String needle = "sad";
+        System.out.println(FirstOccurrenceIndex.strStr(haystack, needle));
+    }
+
+}
