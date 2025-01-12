611c4c62ae3dc01cd35d548b201d27c44781597c
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2025-01-12 13:39:35 -0500

    star real project

---
+++ src/test/java/com/cristian/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cristian.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class LeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() { 
+		assertEquals(true, true);
+	}
+
+}
